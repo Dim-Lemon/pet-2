9e62f3beb71f2d04453d888643ff98f18f41aa8c
Dmitry Kryshen <kryshen.d@mail.ru>
2020-04-20 18:56:57 +0300

    v 0.2.3

--- app/src/main/java/com/example/demo/MapActivity.java
+++ app/src/main/java/com/example/demo/MapActivity.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapActivity extends AppCompatActivity {
 
@@ -16,13 +18,14 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-
+        createMapView();
+        addMarker();
     }
 
     private void createMapView(){
         try {
             if(null == googleMap){
-                googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.fragment_map)).getMap();
+                googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.mapView)).getMap();
                 if(null == googleMap) {
                     Toast.makeText(getApplicationContext(),
                             "Error creating map",Toast.LENGTH_SHORT).show();
@@ -30,6 +33,16 @@ public class MapActivity extends AppCompatActivity {
             }
         } catch (NullPointerException exception){
             Log.e("mapApp", exception.toString());
+        }
+    }
+
+    private void addMarker(){
+        if(null != googleMap){
+            googleMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(0, 0))
+                    .title("Marker")
+                    .draggable(true)
+            );
         }
     }
 }
